6f481014cb8d1568c650c8d65228c12636c10de8
Muhammad Mahfoudh AL-Zubairi <mohammedmahfoudh44@gmail.com>
2025-11-29 03:18:51 +0000

    When I move the phone horizontal it restart the system. stop this

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import * as React from "react"
 
 const MOBILE_BREAKPOINT = 768
@@ -6,13 +8,13 @@ export function useIsMobile() {
   const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
 
   React.useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-    const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    }
-    mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    return () => mql.removeEventListener("change", onChange)
+    const userAgent =
+      typeof window.navigator === "undefined" ? "" : navigator.userAgent
+    const mobile =
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        userAgent
+      )
+    setIsMobile(mobile || window.innerWidth < MOBILE_BREAKPOINT)
   }, [])
 
   return !!isMobile
