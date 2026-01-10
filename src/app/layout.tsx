12f087049d5be8690bdf150308a541c5fc4048b1
Muhammad Mahfoudh AL-Zubairi <mohammedmahfoudh44@gmail.com>
2026-01-10 05:26:35 +0000

    Try fixing this error: `Runtime Error: useLanguage must be used within a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { MealLayout } from "@/components/layout/MealLayout";
+import { LanguageProvider } from "@/context/language-context";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -15,7 +16,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <MealLayout>{children}</MealLayout>
+        <LanguageProvider>
+          <MealLayout>{children}</MealLayout>
+        </LanguageProvider>
       </body>
     </html>
   );
