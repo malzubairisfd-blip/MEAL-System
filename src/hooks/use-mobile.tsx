3fe986731632f23c71bba696390d0c4552f3b848
Muhammad Mahfoudh AL-Zubairi <mohammedmahfoudh44@gmail.com>
2026-01-11 21:11:01 +0000

    Remove the mobile site for the project even if using phone keep it deskt

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -5,17 +5,6 @@ import * as React from "react"
 const MOBILE_BREAKPOINT = 768
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
-
-  React.useEffect(() => {
-    const userAgent =
-      typeof window.navigator === "undefined" ? "" : navigator.userAgent
-    const mobile =
-      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-        userAgent
-      )
-    setIsMobile(mobile || window.innerWidth < MOBILE_BREAKPOINT)
-  }, [])
-
-  return !!isMobile
+  // Always return false to force desktop view
+  return false
 }
