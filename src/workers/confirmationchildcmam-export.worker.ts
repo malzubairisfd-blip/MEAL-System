32ee66b78738f98374736f78e20bb5fe554c5d5e
Muhammad Mahfoudh AL-Zubairi <mohammedmahfoudh44@gmail.com>
2026-03-25 01:47:39 +0000

    Make everything of filtering the data on page only the move the html con

--- src/workers/confirmationchildcmam-export.worker.ts
+++ src/workers/confirmationchildcmam-export.worker.ts
@@ -1,9 +1,6 @@
-
 // This worker is deprecated.
-// The logic has been moved to /src/lib/confirmationchildcmam-export.ts
+// The logic has been moved to src/lib/confirmationchildcmam-export.ts
 // The page component now generates the PDF in the browser using html2pdf.js.
 self.onmessage = () => {
   self.postMessage({ type: 'error', error: 'This worker is deprecated.' });
 };
-
-    
